9f19454af6812049d46f5c833c67bdf9fd7c4ea8
MasakiFukunishi <fmsknn@gmail.com>
2023-08-12 23:47:42 -0700

    Fix routing

---
+++ src/pages/recipes/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Recipes() {
+  return (
+    <>
+      <Head>
+        <title>Recipes</title>
+      </Head>
+      <div>Recipes</div>
+    </>
+  );
+}
---
+++ src/pages/shopping-list/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function ShoppingList() {
+  return (
+    <>
+      <Head>
+        <title>ShoppingList</title>
+      </Head>
+      <div>ShoppingList</div>
+    </>
+  );
+}
