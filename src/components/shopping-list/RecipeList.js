3e9df98571a391dabbef8190a5fe636b969cbf15
MasakiFukunishi <fmsknn@gmail.com>
2023-08-16 09:53:12 -0700

    get data from redux

--- src/components/shopping-list/RecipeList.js
+++ src/components/shopping-list/RecipeList.js
@@ -1,21 +1,8 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
 
 import Recipe from "./Recipe";
 
-function RecipeList() {
-  // const recipes = useSelector((state) => state.recipes.items);
-  const recipes = [
-    {
-      title: "Pasta",
-      image: "https://picsum.photos/id/237/200/300",
-    },
-    {
-      title: "Pizza",
-      image: "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
-    },
-  ];
+function RecipeList({ recipes }) {
   return (
     <ul>
       {recipes.map((recipe) => (
--- src/pages/shopping-list/index.js
+++ src/pages/shopping-list/index.js
@@ -1,18 +1,21 @@
 import Head from "next/head";
 import ItemsToBuy from "@/components/ItemsToBuy";
+import { useSelector } from "react-redux";
 import { recipe } from "./practice";
 
 import Header from "../../components/Header";
 import RecipeList from "../../components/shopping-list/RecipeList";
 
 export default function ShoppingList() {
+  const recipes = useSelector((state) => state.recipes.recipes);
+
   return (
     <>
       <Head>
         <title>ShoppingList</title>
       </Head>
       <Header />
-      <RecipeList />
+      <RecipeList recipes={recipes} />
       {/* I'm placing this here for now/michiru */}
       <ItemsToBuy recipe={recipe} />
     </>
