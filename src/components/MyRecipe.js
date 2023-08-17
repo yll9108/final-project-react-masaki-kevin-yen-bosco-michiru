0f00cf6057449b8a02279839fd1ac68c1c4595f0
michiru6828 <since_l999@yahoo.co.jp>
2023-08-17 11:30:08 -0700

    added provider to set localstorage items to redux when page is refreshed

--- src/components/MyRecipe.js
+++ src/components/MyRecipe.js
@@ -1,33 +1,29 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react'
+import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import { removeFromMyRecipes } from '@/store/slicers/myReceips'
 
 function MyRecipes() {
-    const myRecipes = useSelector(state => state.recipes.recipes);
-    const dispatch = useDispatch()
-    const handleOnClick = (recipeId) => {
-      dispatch(removeFromMyRecipes(recipeId))
-    }
-    console.log(myRecipes)
-    useEffect(() => {
-    
-        console.log('MyRecipes has changed:', myRecipes);
-    }, [myRecipes]);
+  const myRecipes = useSelector((state) => state.recipes.recipes)
+  const dispatch = useDispatch()
+  const handleOnClick = (recipeId) => {
+    dispatch(removeFromMyRecipes(recipeId))
+  }
 
-    return (
-        <div>
-            <h2>My Recipes</h2>
-            <ul>
-                {myRecipes && myRecipes.map((recipe, index) => (
-                    <li key={index}>
-                        <h3>{recipe.title}</h3>
-                        <button onClick={() => handleOnClick(recipe.id)}>Remove</button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h2>My Recipes</h2>
+      <ul>
+        {myRecipes &&
+          myRecipes.map((recipe, index) => (
+            <li key={index}>
+              <h3>{recipe.title}</h3>
+              <button onClick={() => handleOnClick(recipe.id)}>Remove</button>
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
 }
 
-export default MyRecipes;
+export default MyRecipes
--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import myFridge from './slicers/myFridge';
-import myReceips from './slicers/myReceips';
-import itemsToGet from './slicers/itemsToGet';
-
+import { configureStore } from '@reduxjs/toolkit'
+import myFridge from './slicers/myFridge'
+import myReceips from './slicers/myReceips'
 const store = configureStore({
   reducer: {
     fridge: myFridge,
     recipes: myReceips,
-    itemsToGet: itemsToGet,
   },
-});
+})
 
-export default store;
+export default store
