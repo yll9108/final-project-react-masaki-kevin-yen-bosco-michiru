ae253c8fbcd465e47ba74318a6b84e9bab299511
YenLun <yenlunliu@gmail.com>
2023-08-17 16:48:22 -0700

    cuisine section filtering is working

--- src/components/FilterArea.js
+++ src/components/FilterArea.js
@@ -1,6 +1,7 @@
+import { axiosInstance } from "@/axios";
 import axios from "axios";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const availableCuisines = [
     "African",
@@ -34,6 +35,25 @@ const availableCuisines = [
 
 export default function FilterArea() {
     const [selectedCuisines, setSelectedCuisines] = useState([]);
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        // 在选中的复选框内容发生变化时，执行获取食谱的操作
+        const fetchRecipes = async () => {
+            try {
+                const response = await axios.get(
+                    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${
+                        process.env.NEXT_PUBLIC_SPOONACULAR_APIKEY
+                    }&cuisine=${selectedCuisines.join(",")}&number=5`
+                );
+                setRecipes(response.data.results);
+            } catch (error) {
+                console.log("API ERROR", error);
+            }
+        };
+
+        fetchRecipes();
+    }, [selectedCuisines]);
 
     const handleCheckboxChange = (value) => {
         setSelectedCuisines((prevSelected) => {
@@ -45,44 +65,34 @@ export default function FilterArea() {
         });
     };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try {
-            const response = await axios.get(
-                `https://api.spoonacular.com/recipes/complexSearch?${
-                    process.env.NEXT_PUBLIC_SPOONACULAR_APIKEY
-                }&cuisine=${selectedCuisines.join(",")}&number=5`
-            );
-        } catch (error) {
-            console.log("API ERROR", error);
-        }
-    };
-
     return (
         <>
             <div>
-                <form onSubmit={handleSubmit}>
-                    <h1>FilterArea</h1>
-                    <label>CUISINE</label>
-                    <div>
-                        {availableCuisines.map((cuisine) => (
-                            <label key={cuisine}>
-                                <input
-                                    type="checkbox"
-                                    value={cuisine}
-                                    checked={selectedCuisines.includes(cuisine)}
-                                    onChange={() =>
-                                        handleCheckboxChange(cuisine)
-                                    }
-                                />
-                                {cuisine}
-                            </label>
-                        ))}
-                    </div>
-                    <h3>DIET</h3>
-                    <h3>INTOLERENCES</h3>
-                </form>
+                <h1>FilterArea</h1>
+                <label>CUISINE</label>
+                <div>
+                    {availableCuisines.map((cuisine) => (
+                        <label key={cuisine}>
+                            <input
+                                type="checkbox"
+                                value={cuisine}
+                                checked={selectedCuisines.includes(cuisine)}
+                                onChange={() => handleCheckboxChange(cuisine)}
+                            />
+                            {cuisine}
+                        </label>
+                    ))}
+                </div>
+                <h3>DIET</h3>
+                <h3>INTOLERENCES</h3>
+            </div>
+            <div>
+                <h2>Recipes</h2>
+                <ul>
+                    {recipes.map((recipe) => (
+                        <li key={recipe.id}>{recipe.title}</li>
+                    ))}
+                </ul>
             </div>
         </>
     );
