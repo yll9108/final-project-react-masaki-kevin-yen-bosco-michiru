2811e9219aedf9770ff59ca0f7f96206db91da14
MasakiFukunishi <fmsknn@gmail.com>
2023-08-18 09:46:58 -0700

    Add slicer

---
+++ src/store/slicers/search.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  myFridgeIngredients: [],
+  searchRecipe: [],
+  filterCuisine: [],
+  filterDiet: [],
+  filterIntolerance: [],
+};
