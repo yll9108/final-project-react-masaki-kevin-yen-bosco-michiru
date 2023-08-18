4b3089ec983629244882c92114a84928c1a3f333
YenLun <yenlunliu@gmail.com>
2023-08-18 11:51:03 -0700

    filtering area working

--- src/components/FilterArea.js
+++ src/components/FilterArea.js
@@ -64,16 +64,23 @@ const availableIntolerances = [
 
 export default function FilterArea({ setRecipes }) {
     const [selectedCuisines, setSelectedCuisines] = useState([]);
+    const [selectedDiet, setSelectedDiet] = useState([]);
+    const [selectedIntolerances, setSelectedIntolerances] = useState([]);
     // const [recipes, setRecipes] = useState([]);
 
     useEffect(() => {
         // 在选中的复选框内容发生变化时，执行获取食谱的操作
+
         const fetchRecipes = async () => {
             try {
                 const response = await axios.get(
                     `https://api.spoonacular.com/recipes/complexSearch?apiKey=${
                         process.env.NEXT_PUBLIC_SPOONACULAR_APIKEY
-                    }&cuisine=${selectedCuisines.join(",")}&number=10`
+                    }&cuisine=${selectedCuisines.join(
+                        ","
+                    )}&diet=${selectedDiet.join(
+                        ","
+                    )}&intolerances=${selectedIntolerances.join(",")}&number=1`
                 );
                 setRecipes(response.data.results);
             } catch (error) {
@@ -82,9 +89,9 @@ export default function FilterArea({ setRecipes }) {
         };
 
         fetchRecipes();
-    }, [selectedCuisines]);
+    }, [selectedCuisines, selectedDiet, selectedIntolerances]);
 
-    const handleCheckboxChange = (value) => {
+    const handleCuisineCheckboxChange = (value) => {
         setSelectedCuisines((prevSelected) => {
             if (prevSelected.includes(value)) {
                 return prevSelected.filter((item) => item !== value);
@@ -92,6 +99,32 @@ export default function FilterArea({ setRecipes }) {
                 return [...prevSelected, value];
             }
         });
+        console.log("Selected Cuisine After", selectedDiet);
+        console.log("New Cuisine Value", selectedDiet);
+    };
+
+    const handleDietCheckboxChange = (value) => {
+        setSelectedDiet((prevSelected) => {
+            if (prevSelected.includes(value)) {
+                return prevSelected.filter((item) => item !== value);
+            } else {
+                return [...prevSelected, value];
+            }
+        });
+        console.log("Selected Intolerances After", selectedIntolerances);
+        console.log("New Intolerance Value", value);
+    };
+
+    const handleIntolerancesCheckboxChange = (value) => {
+        setSelectedIntolerances((prevSelected) => {
+            if (prevSelected.includes(value)) {
+                return prevSelected.filter((item) => item !== value);
+            } else {
+                return [...prevSelected, value];
+            }
+        });
+        console.log("Selected Diet After", selectedIntolerances);
+        console.log("New Diet Value", value);
     };
 
     return (
@@ -106,14 +139,48 @@ export default function FilterArea({ setRecipes }) {
                                 type="checkbox"
                                 value={cuisine}
                                 checked={selectedCuisines.includes(cuisine)}
-                                onChange={() => handleCheckboxChange(cuisine)}
+                                onChange={() =>
+                                    handleCuisineCheckboxChange(cuisine)
+                                }
                             />
                             {cuisine}
                         </label>
                     ))}
                 </div>
                 <h3>DIET</h3>
+                <div>
+                    {availableDiet.map((diet) => (
+                        <label key={diet}>
+                            <input
+                                type="checkbox"
+                                value={diet}
+                                checked={selectedDiet.includes(diet)}
+                                onChange={() => handleDietCheckboxChange(diet)}
+                            />
+                            {diet}
+                        </label>
+                    ))}
+                </div>
                 <h3>INTOLERENCES</h3>
+                <div>
+                    {availableIntolerances.map((intolerances) => (
+                        <label key={intolerances}>
+                            <input
+                                type="checkbox"
+                                value={intolerances}
+                                checked={selectedIntolerances.includes(
+                                    intolerances
+                                )}
+                                onChange={() =>
+                                    handleIntolerancesCheckboxChange(
+                                        intolerances
+                                    )
+                                }
+                            />
+                            {intolerances}
+                        </label>
+                    ))}
+                </div>
             </div>
         </>
     );
