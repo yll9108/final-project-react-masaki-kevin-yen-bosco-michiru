7b8151d7a8cbc3a1cae84beb32c4aa7f060e2df7
Bosco Lam <121968040+bosco1230@users.noreply.github.com>
2023-08-17 17:10:50 -0700

    show image when clicxked show button

--- src/components/MyRecipe.js
+++ src/components/MyRecipe.js
@@ -1,19 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux'
-import { removeFromMyRecipes } from '@/store/slicers/myReceips'
+import { useDispatch } from 'react-redux';
+import { removeFromMyRecipes } from '@/store/slicers/myReceips';
 
 function MyRecipes() {
     const myRecipes = useSelector(state => state.recipes.recipes);
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [showImage, setShowImage] = useState(false); // State to manage image visibility
+
     const handleOnClick = (recipeId) => {
-      dispatch(removeFromMyRecipes(recipeId))
+      dispatch(removeFromMyRecipes(recipeId));
     }
-    console.log(myRecipes)
-    useEffect(() => {
-    
-        console.log('MyRecipes has changed:', myRecipes);
-    }, [myRecipes]);
+
+    const toggleImage = (recipeId, recipeImage) => {
+        if (selectedImage === recipeImage) {
+            setSelectedImage(null);
+            setShowImage(false);
+        } else {
+            setSelectedImage(recipeImage);
+            setShowImage(true);
+        }
+    }
 
     return (
         <div>
@@ -23,6 +31,14 @@ function MyRecipes() {
                     <li key={index}>
                         <h3>{recipe.title}</h3>
                         <button onClick={() => handleOnClick(recipe.id)}>Remove</button>
+                        <button onClick={() => toggleImage(recipe.id, recipe.image)}>
+                            {showImage && selectedImage === recipe.image ? 'Hide' : 'Show'}
+                        </button>
+                        {showImage && selectedImage === recipe.image && (
+                            <div>
+                                <img src={selectedImage} alt={recipe.title} />
+                            </div>
+                        )}
                     </li>
                 ))}
             </ul>
