934691724fef080285b61920e3b877b17de7eb49
Kevin Lee <kevinlfcola@gmail.com>
2023-08-19 00:41:47 -0700

    Header basic style done

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,89 @@ import Link from "next/link";
 
 import useAuth from "../hooks/useAuth";
 import { login, logout } from "../lib/auth";
+import { styled } from "styled-components";
 
 function Header() {
-  const user = useAuth();
-  return (
-    <>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="recipes">Resipes</Link>
-        </li>
-        <li>
-          <Link href="shopping-list">Shopping List</Link>
-        </li>
-      </ul>
+    const user = useAuth();
 
-      {user ? <button onClick={logout}>Logout</button> : <button onClick={login}>Login</button>}
-    </>
-  );
+    //Style
+    const Header = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin: 10px 0;
+    `;
+
+    const CompanyName = styled.h1`
+        margin: 0 20px;
+    `;
+
+    const HeaderUl = styled.ul`
+        display: flex;
+        flex-direction: row;
+        list-style-type: none;
+        margin: 0;
+    `;
+
+    const HeaderLi = styled.li`
+        margin: 0 10px;
+        cursor: pointer;
+    `;
+
+    const LoginBtn = styled.button`
+        margin: 0 20px;
+        background: none;
+        border: none;
+        appearance: none;
+        cursor: pointer;
+    `;
+
+    const LogoutBtn = styled.button`
+        margin: 0 20px;
+        background: none;
+        border: none;
+        appearance: none;
+        cursor: pointer;
+    `;
+
+    return (
+        <Header>
+            <CompanyName>FIRDGEFY</CompanyName>
+            <HeaderUl>
+                <HeaderLi>
+                    <Link
+                        href="/"
+                        style={{ textDecoration: "none", color: "inherit" }}
+                    >
+                        Home
+                    </Link>
+                </HeaderLi>
+                <HeaderLi>
+                    <Link
+                        href="recipes"
+                        style={{ textDecoration: "none", color: "inherit" }}
+                    >
+                        Resipes
+                    </Link>
+                </HeaderLi>
+                <HeaderLi>
+                    <Link
+                        href="shopping-list"
+                        style={{ textDecoration: "none", color: "inherit" }}
+                    >
+                        Shopping List
+                    </Link>
+                </HeaderLi>
+            </HeaderUl>
+
+            {user ? (
+                <LogoutBtn onClick={logout}>Logout</LogoutBtn>
+            ) : (
+                <LoginBtn onClick={login}>Login</LoginBtn>
+            )}
+        </Header>
+    );
 }
 
 export default Header;
--- src/components/MyFridge.js
+++ src/components/MyFridge.js
@@ -1,12 +1,24 @@
 import React from "react";
 import MyFridgeSearch from "./MyFridgeSearch";
 import MyFridgeItems from "./MyFridgeItems";
+import styled from "styled-components";
 
 export default function MyFridge() {
+    const MyFridge = styled.div`
+        display: flex;
+        flex-direction: column;
+        border: 1px solid black;
+        border-radius: 5px;
+        margin: 10px;
+        padding: 0;
+    `;
+
     return (
-        <div>
+        <MyFridge>
+            <h1>My Fridge</h1>
             <MyFridgeSearch />
             <MyFridgeItems />
-        </div>
+            <p>Selete items to filter the search</p>
+        </MyFridge>
     );
 }
