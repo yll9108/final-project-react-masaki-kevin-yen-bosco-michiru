1266fcc8f38cd174f198cfb15272d5853ba04ec4
Kevin Lee <kevinlfcola@gmail.com>
2023-08-21 01:16:15 -0700

    Shopping list basic style done

--- src/components/shopping-list/Recipe.js
+++ src/components/shopping-list/Recipe.js
@@ -1,38 +1,93 @@
 import { useState } from "react";
 import { removeFromMyRecipes } from "@/store/slicers/myReceips";
 import { useDispatch } from "react-redux";
+import { styled } from "styled-components";
+import { AiFillDelete } from "react-icons/ai";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 function Recipe({ recipe, setSelectedRecipe }) {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const dispatch = useDispatch();
+    const [isExpanded, setIsExpanded] = useState(false);
+    const dispatch = useDispatch();
 
-  const remove = (recipe) => {
-    dispatch(removeFromMyRecipes(recipe.id));
-    setSelectedRecipe(null);
-  };
+    const remove = (recipe) => {
+        dispatch(removeFromMyRecipes(recipe.id));
+        setSelectedRecipe(null);
+    };
 
-  const toggleAccordion = () => {
-    setIsExpanded((prevExpanded) => !prevExpanded);
-  };
-  return (
-    <li>
-      <h2
-        style={{
-          cursor: "pointer",
-        }}
-        onClick={() => setSelectedRecipe(recipe)}
-      >
-        {recipe.title}
-      </h2>
-      <button onClick={() => remove(recipe)}>Remove</button>
-      {isExpanded ? <button onClick={toggleAccordion}>Hide</button> : <button onClick={toggleAccordion}>Show</button>}
-      {isExpanded && (
-        <div>
-          <img src={recipe.image} alt={recipe.title} />
-        </div>
-      )}
-    </li>
-  );
+    const toggleAccordion = () => {
+        setIsExpanded((prevExpanded) => !prevExpanded);
+    };
+
+    //Style
+    const RecipesList = styled.li`
+        border: 1px solid black;
+        list-style-type: none;
+        padding: 20px;
+        margin: 10px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 700px;
+    `;
+
+    const RecipesTitle = styled.p`
+        margin: 0;
+        font-size: 20px;
+    `;
+
+    const RemoveBtn = styled.button`
+        font-size: 25px;
+        color: red;
+        background: none;
+        border: none;
+        cursor: pointer;
+    `;
+
+    const ShowBtn = styled.button`
+        font-size: 25px;
+        background: none;
+        border: none;
+        cursor: pointer;
+    `;
+
+    const HideBtn = styled.button`
+        font-size: 25px;
+        background: none;
+        border: none;
+        cursor: pointer;
+    `;
+
+    return (
+        <RecipesList>
+            <RecipesTitle
+                style={{
+                    cursor: "pointer",
+                }}
+                onClick={() => setSelectedRecipe(recipe)}
+            >
+                {recipe.title}
+            </RecipesTitle>
+            <div>
+                <RemoveBtn onClick={() => remove(recipe)}>
+                    <AiFillDelete />
+                </RemoveBtn>
+                {isExpanded ? (
+                    <HideBtn onClick={toggleAccordion}>
+                        <IoIosArrowUp />
+                    </HideBtn>
+                ) : (
+                    <ShowBtn onClick={toggleAccordion}>
+                        <IoIosArrowDown />
+                    </ShowBtn>
+                )}
+            </div>
+            {isExpanded && (
+                <div>
+                    <img src={recipe.image} alt={recipe.title} />
+                </div>
+            )}
+        </RecipesList>
+    );
 }
 
 export default Recipe;
--- src/pages/shopping-list/index.js
+++ src/pages/shopping-list/index.js
@@ -5,10 +5,18 @@ import { useState } from "react";
 import Header from "../../components/Header";
 import MyFridge from "@/components/MyFridge";
 import RecipeList from "../../components/shopping-list/RecipeList";
+import { styled } from "styled-components";
 
 export default function ShoppingList() {
     const recipes = useSelector((state) => state.recipes.recipes);
     const [selecedRecipe, setSelectedRecipe] = useState(null);
+
+    //Style
+    const ShoppingListPage = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    `;
 
     return (
         <>
@@ -16,12 +24,14 @@ export default function ShoppingList() {
                 <title>ShoppingList</title>
             </Head>
             <Header />
-            <MyFridge />
-            <RecipeList
-                recipes={recipes}
-                setSelectedRecipe={setSelectedRecipe}
-            />
-            <ItemsToBuy recipe={selecedRecipe} />
+            <ShoppingListPage>
+                <MyFridge />
+                <RecipeList
+                    recipes={recipes}
+                    setSelectedRecipe={setSelectedRecipe}
+                />
+                <ItemsToBuy recipe={selecedRecipe} />
+            </ShoppingListPage>
         </>
     );
 }
